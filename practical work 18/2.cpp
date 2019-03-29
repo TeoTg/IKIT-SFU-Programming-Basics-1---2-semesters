16a4a097920135f4d84638f92b2112334faab8ff
zloysergunya <30868885+zloysergunya@users.noreply.github.com>
2019-03-29 10:34:23 +0700

    Add files via upload

---
+++ practical work 18/2.cpp
@@ -0,0 +1,98 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <clocale>
+#include <ctime>
+
+using namespace std;
+
+struct node {
+	int info;
+	node *link;
+};
+
+// Вывод очереди на экран
+void print(node *k) {
+	node *new_node = k;
+	while (new_node != NULL) {
+		cout << new_node->info << " ";
+		new_node = new_node->link;
+	}
+}
+
+// Созднание узла
+node *create_node(int num) {
+	node *f = new node;
+	f->link = nullptr;
+	f->info = num;
+	return f;
+}
+
+// Добавление узла
+node *add_node(int num) {
+	node *f = new node;
+	f->link = nullptr;
+	f->info = 1 + rand() % num;
+	return f;
+}
+
+// Проверка числа на совершенность
+bool perfectNum_check(int num) {
+	if (num == 0) {
+		return false;
+	}
+	int sum = 0;
+	for (int i = num / 2; i >= 1; --i) {
+		if (num % i == 0) {
+			sum += i;
+		}
+	}
+	return sum == num;
+}
+
+int main() {
+	setlocale(LC_ALL, "");
+	srand(time(NULL));
+
+	int n, t;
+	cout << "Введите количество чисел: ";
+	cin >> n;
+	cout << "Введите сами числа: ";
+	cin >> t;
+
+	node *l = nullptr, *r = nullptr, *k = nullptr, *newnode = nullptr;
+	k = create_node(t);
+	l = r = k;
+
+	for (int i = 1; i < n; i++) {
+		cin >> t;
+		k = create_node(t);
+		r->link = k;
+		r = k;
+	}
+	k = l;
+
+	node *last = k;
+	while (k != nullptr) {
+		if (perfectNum_check(k->info)) {
+			for (int j = 0; j < 5; j++) {
+				newnode = add_node(k->info);
+				if (k == l) {
+					newnode->link = k;
+					l = last = newnode;
+				}
+				else {
+					newnode->link = last->link;
+					last->link = newnode;
+				}
+			}
+		}
+		last = k;
+		k = k->link;
+	}
+	k = l;
+
+	cout << endl << "Получившийся список: ";
+	print(k);
+
+	return 0;
+}
---
+++ practical work 19/1.cpp
@@ -0,0 +1,130 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <clocale>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+struct student {
+	char surname[20];
+	int age;
+	int course;
+	bool department;
+	student *link;
+};
+
+// Созднание узла
+student *create_node(ifstream &fin) {
+	student *f = new student;
+	f->link = nullptr;
+	fin >> f->surname >> f->age >> f->course;
+	char department[20];
+	fin >> department;
+	f->department = !strcmp(department, "Да");
+	return f;
+}
+
+// Проверка данных на удовлетворения условию
+bool student_filtering(student *k) {
+	return k->course == 4 && k->department == true;
+}
+
+// Запись данных в двоичный файл
+void write_data_binar(fstream &fout, student *k) {
+	fout.write((char*)k, sizeof(student));
+}
+
+// Чтение данных из двоичного файла
+void read_data_binar(fstream &fout) {
+	student *buf = new student;
+	fout.read((char *)buf, sizeof(student));
+	cout.setf(ios::left);
+	cout << setw(20) << buf->surname << setw(20);
+	cout << buf->age << setw(20);
+	cout << buf->course << setw(20);
+	if (buf->department) {
+		cout << "Да" << endl;
+	}
+	else {
+		cout << "Нет" << endl;
+	}
+	delete[] buf;
+}
+
+// Вывод файла на экран
+void print_console(student *k) {
+	cout.setf(ios::left);
+	cout << setw(20) << k->surname << setw(20) << k->age << setw(20) << k->course << setw(20);
+	if (k->department) {
+		cout << "Да" << endl;
+	}
+	else {
+		cout << "Нет" << endl;
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "");
+
+	ifstream fin("in.txt");
+	fstream fout("out.bin", ios::binary | ios::out | ios::in | ios::trunc);
+
+	if (!fin.is_open()) {
+		cout << "Входящий файл не найден! " << endl;
+		return -1;
+	}
+
+	student *l = nullptr, *r = nullptr, *k = nullptr;
+	int n = 0; // Количество узлов
+
+	k = create_node(fin);
+	l = r = k;
+
+	while (!fin.eof()) {
+		k = create_node(fin);
+		r->link = k;
+		r = k;
+		n++;
+	}
+	k = l;
+
+	cout << "Содержимое входного файла: " << endl;
+	for (int i = 0; i < n; i++) {
+		print_console(k);
+		r->link = k;
+		r = k;
+		k = k->link;
+	}
+	k = l;
+
+	int size_sort_buf = 0;
+	fout.seekp(0, ios_base::beg);
+	for (int i = 0; i < n; i++) {
+		if (student_filtering(k)) {
+			write_data_binar(fout, k);
+			size_sort_buf++;
+		}
+		r->link = k;
+		r = k;
+		k = k->link;
+	}
+	k = l;
+
+	if (size_sort_buf == 0) {
+		cout << endl << "Нет студентов, которые посещают военную кафедру. " << endl;
+	}
+	else {
+		cout << endl << "Студенты 4 курса, которые посещают военную кафедру: " << endl;
+		fout.seekg(0, ios_base::beg);
+		for (int i = 0; i < size_sort_buf; i++) {
+			read_data_binar(fout);
+		}
+	}
+
+	fout.close();
+	fin.close();
+	cout << endl << "Пришло время проверить выходной файл! " << endl;
+	return 0;
+}
